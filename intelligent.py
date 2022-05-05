f396e5708dddefb8cbbda2cb669fcc786d3f78fc
Chaeeun Park <chaeeun0618@unm.edu>
2022-05-04 18:04:38 -0600

    1

--- intelligent.py
+++ intelligent.py
@@ -5,6 +5,7 @@ from statistics import mean
 from tkinter import *
 
 
+
 class Field:
     WIDTH = 1000
     HEIGHT = 750
@@ -19,21 +20,19 @@ class Coordinate():
         self.y = 0
 
 
-
 class Bird():
     NUM = 40 
     RADIAN = 3
     SPEED = 4
 
-    @staticmethod
-    def setup(args):
-        Bird.birds = [Bird(args) for _ in range(Bird.NUM)]
-
+    # 생성자
     def __init__(self, args):
-        self.x = random.randint(0, Field.WIDTH)
-        self.y = random.randint(0, Field.HEIGHT)
-        self.vx = random.randint(-self.SPEED, self.SPEED)
-        self.vy = random.randint(-self.SPEED, self.SPEED)
+        self.x = random.randint(50, 100) #start position
+        self.y = random.randint(350, 400)
+#        self.vx = random.randint(-self.SPEED, self.SPEED)
+#        self.vy = random.randint(-self.SPEED, self.SPEED)
+        self.vx = 0
+        self.vy = 0
         self.r1 = args.r1
         self.r2 = args.r2
         self.r3 = args.r3
@@ -63,7 +62,7 @@ class Bird():
         self.v1.y = (self.v1.y - self.y) / self.center_pull
 
     def separation(self):
-        personal_space = 15
+        personal_space = 10
         for bird in self.neighbors:
             if bird == self:
                 continue
@@ -84,6 +83,15 @@ class Bird():
         self.v3.x = (self.v3.x - self.vx) / 2
         self.v3.y = (self.v3.y - self.vy) / 2
 
+    def intelligent_direction(self):
+        #종착지에 맞춰서 계속 바꾸기
+        end_x = Field.WIDTH
+        end_y = Field.HEIGHT
+
+        self.vx = (end_x-self.x)
+            
+        self.v3.x = self.vx/self.SPEED
+
     def _collision_detection(self):
         if self.x - Bird.RADIAN < 0:
             self.vx *= -1
@@ -103,6 +111,11 @@ class Bird():
         self.cohesion()
         self.separation()
         self.alignment()
+
+    def intelligent_step(self):
+        self.cohesion()
+        self.separation()
+        self.intelligent_direction()
 
     def update(self):
         dx = self.r1 * self.v1.x + self.r2 * self.v2.x + self.r3 * self.v3.x
@@ -133,20 +146,40 @@ class Bird():
         drawer.create_oval(self.x - Bird.RADIAN, self.y - Bird.RADIAN, self.x + Bird.RADIAN, self.y + Bird.RADIAN)
         drawer.create_line(self.x, self.y, self.x + self.vx * 3, self.y + self.vy * 3)
 
+    def intelligentdraw(self, drawer):
+        self.clear_movement()
+        self.intelligent_step()
+        self.update()
+        drawer.create_oval(self.x - Bird.RADIAN, self.y - Bird.RADIAN, self.x + Bird.RADIAN, self.y + Bird.RADIAN, fill = "red")
+        drawer.create_line(self.x, self.y, self.x + self.vx * 3, self.y + self.vy * 3)
 
 def main(args):
-    Bird.setup(args)
-
+    Bird.birds = [Bird(args) for _ in range(Bird.NUM)]
+    
     root = Tk()
 
     canvas = Canvas(root, width=Field.WIDTH, height=Field.HEIGHT)
     canvas.pack()
 
+#   Bird.birds[0].x = 1
+#   Bird.birds[0].y = 1
+
+#   Bird.birds[1].x = 1
+#   Bird.birds[1].y = 100
+
+#   Bird.birds[2].x = 100
+#   Bird.birds[2].y = 1
+
+#    Bird.birds[3].x = 100
+#    Bird.birds[3].y = 100
+
     def animate():
         canvas.delete("all")
-        for bird in Bird.birds:
+        for bird in Bird.birds:    
             bird.find_neighbors()
             bird.draw(canvas)
+        
+        Bird.birds[0].intelligentdraw(canvas)
         root.after(20, animate)
 
     animate()
@@ -155,7 +188,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Boids Model parameters')
-    parser.add_argument('--r1', type=float, default=1.0, help='cohesion coefficient')
+    parser.add_argument('--r1', type=float, default=2.0, help='cohesion coefficient') #원래는 1.0
     parser.add_argument('--r2', type=float, default=0.8, help='separation coefficient')
     parser.add_argument('--r3', type=float, default=0.3, help='alignment coefficient')
     parser.add_argument('--center-pull', type=int, default=300,
@@ -163,3 +196,5 @@ if __name__ == '__main__':
     parser.add_argument('--view', type=int, default=50, help='view of each birds') #150
     args = parser.parse_args()
     main(args)
+
+    
