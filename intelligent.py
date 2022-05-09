3d690abcd1664228357250f16de8aa86d66c06be
kun <179639879@qq.com>
2022-05-09 14:20:07 -0600

    s

--- intelligent.py
+++ intelligent.py
@@ -4,6 +4,7 @@ import random
 from statistics import mean
 from tkinter import *
 import numpy as np
+import time
 
 class Field:
     WIDTH = 1000
@@ -19,38 +20,96 @@ class Field:
 
 
 class Bird():
-    NUM = 40 
+    NUM = 1
+    smartCount = 1
     RADIAN = 3
     SPEED = 4
-    ACCUP = 1
+    ACCUP = 0.1
     srcSquare = [25, 325, 125, 425]
     desSquare = [875, 325, 975, 425]
-    smartCount = 20
+
     des = np.array([(desSquare[0]+desSquare[2])/2, (desSquare[1]+desSquare[3])/2])
+
+
+
+    ## parameters
+    r = [1] * 10
+    r[0] = 0.25     # cohesion coefficient
+    r[1] = 0.25     # separation coefficient
+    r[2] = 0.2     # alignment coefficient
+    r[3] = 0.25        # intelligent coefficient
+    #r[4]           # acceleration noise
+    center_pull = 30
+    view = 50
+
+
+
+    deviation=[]
+    aveVelocity=[]
+    measurement=[[]for i in range(5)]
+    measurement={
+        'deviation':[],
+        'avevel':[],                # average velocity
+        'aveprovel':[],     # average velocity projected to the direction to the destination
+        'allarrival':[]
+    }
     # 생성자
     def __init__(self, args):
-        # self.x = random.randint(50, 100) #start position
-        # self.y = random.randint(350, 400)
-        self.pos = np.random.rand(1)*50+np.array([50, 350])
-#        self.vx = random.randint(-self.SPEED, self.SPEED)
-#        self.vy = random.randint(-self.SPEED, self.SPEED)
-#         self.vx = 0
-#         self.vy = 0
+
+        self.pos = np.random.rand(2)*50+np.array([50, 350])
+        #self.poss=[]
         self.v = np.zeros(2)
 
-        self.r=[0]*10
-        self.r[0] = args.r0
-        self.r[1] = args.r1
-        self.r[2] = args.r2
-        self.r[3] = args.r3
-        self.center_pull = args.center_pull
-        self.view = args.view
-        self.neighbors = None
+        # self.r=[0]*10
+        # self.r[0] = args.r0
+        # self.r[1] = args.r1
+        # self.r[2] = args.r2
+        # self.r[3] = args.r3
+        # self.center_pull = args.center_pull
+        # self.view = args.view
+
+        self.neighbors = []
         self.acceleration = 0
         self.smartFlag=0
 
+        self.arriveFlag=0
         self.vf = [np.zeros(2)]*10
 
+    @classmethod
+    def neighbors(clc):
+        disTable=np.ones([clc.NUM,clc.NUM])*(clc.view+1)
+        for i,first in enumerate(Bird.birds):
+            for j,second in enumerate(Bird.birds[i+1:]):
+                j += i+1
+                disTable[i,j]=disTable[j,i]=np.linalg.norm(first.pos-second.pos)
+            for i,val in enumerate(disTable[i,:]):
+                if val<clc.view:
+                    first.neighbors.append(clc.birds[i])
+
+
+    @classmethod
+    def measure(clc):
+        poss= [clc.birds[i].pos for i in range(clc.NUM)]    #record all birds' position
+        dev=np.std(poss)
+        clc.measurement['deviation'].append(dev)
+
+        vels=[clc.birds[i].v for i in range(clc.NUM)]
+        velsnorm=np.linalg.norm(vels,axis=1)
+        velsnormave=np.average(velsnorm)
+        clc.measurement['avevel'].append(velsnormave)
+
+        #provels = [clc.birds[i].v for i in range(clc.NUM)]
+        vec2des=poss-clc.des
+        vec2desnorm=np.linalg.norm(vec2des,axis=1)
+
+        # projection velocity
+        projvels=[np.dot(vels[i],vec2des[i])/vec2desnorm[i]**2*vec2des[i] for i in range(clc.NUM)]
+        projvelsnorm=np.linalg.norm(projvels,axis=1)
+        projvelsnormave=np.average(projvelsnorm)
+        clc.measurement['avevel'].append(projvelsnormave)
+
+    def dummtest(self):
+        print(1)
 
     def find_neighbors(self):
         self.neighbors = [agent for agent in self.birds if agent is not self and self.calc_distance(agent) <= self.view]
@@ -60,18 +119,13 @@ class Bird():
         return np.linalg.norm(target.pos-self.pos)
 
     def cohesion(self):
-        if not self.neighbors:
-            return
-        #self.v1.x = mean([agent.x for agent in self.neighbors if agent is not self])
-        #self.v1.y = mean([agent.y for agent in self.neighbors if agent is not self])
 
         self.vf[0] = np.mean([agent.pos for agent in self.neighbors if agent is not self],axis=0)
-
-        # self.v1.x = (self.v1.x - self.x) / self.center_pull
-        # self.v1.y = (self.v1.y - self.y) / self.center_pull
-        self.vf[0] = (self.vf[0]-self.pos)/self.center_pull
+        self.vf[0] = (self.vf[0]-self.pos)#/self.center_pull
+        self.vf[0]=self.normalization(self.vf[0])
 
     def separation(self):
+
         personal_space = 10
         self.vf[1]=np.zeros(2)
         for bird in self.neighbors:
@@ -81,40 +135,41 @@ class Bird():
             if dist == 0:
                 continue
             if dist < personal_space:
-                # self.v2.x -= (bird.x - self.x) / 2
-                # self.v2.y -= (bird.y - self.y) / 2
-                self.vf[1] = self.vf[1] - (bird.pos-self.pos)/2
+                #self.vf[1] = self.vf[1] - (bird.pos-self.pos)
+                dirndis=bird.pos - self.pos
+                dir=self.normalization(dirndis)
+                dis=np.linalg.norm(dirndis)
+                newdis=1/(dis/personal_space)
+                self.vf[1] -= dir*newdis
+                #self.vf[1]=self.normalization(self.vf[1])
 
 
     def alignment(self):
-        if not self.neighbors:
-            return
 
-        # self.v3.x = mean([agent.vx for agent in self.neighbors if agent is not self])
-        # self.v3.y = mean([agent.vy for agent in self.neighbors if agent is not self])
         self.vf[2] = np.mean([agent.v for agent in self.neighbors if agent is not self],axis=0)
+        self.vf[2]=(self.vf[2] - self.v)
+        self.vf[2]=self.normalization(self.vf[2])
 
-        # self.v3.x = (self.v3.x - self.vx) / 2
-        # self.v3.y = (self.v3.y - self.vy) / 2
-        self.vf[2]=(self.vf[2] - self.v) / 2
-    def normalization(self, direct, scaleValue):
+    def noise(self):
+        self.vf[4]=np.zeros([2])*0.05*self.ACCUP
+
+    def normalization(self, direct, scaleValue=1):
         # print(1)
         # k = pow(dx, 2) + pow(dy, 2) + 0.000001
         # b = math.sqrt(k / self.ACCUP)
         # dx_2 = dx / b
         # dy_2 = dy / b
-        norm=np.linalg.norm(direct) + 0.00000001     #   don't be 0
+        norm=np.linalg.norm(direct) + 0.00000000001     #   don't be 0
         return direct/norm*scaleValue
 
 
 
     def smartDirection(self):
-        
-        # self.vf[3]=np.array([1,0])
-        direction = Bird.des-self.pos
-        norm = np.linalg.norm(direction)
-        self.vf[3] = direction/norm
-        # self.v4.x,self.v4.y=self.des_direction(self,[self.x,self.y],self.des)
+        if Bird.desSquare[:2]<list(self.pos)<Bird.desSquare[2:]:
+            direction=-self.v
+        else:
+            direction = Bird.des-self.pos
+        self.vf[3]=self.normalization(direction)
 
     def _collision_detection(self):
         # if self.x - Bird.RADIAN < 0:
@@ -146,44 +201,35 @@ class Bird():
 
 
     def step(self):
+        self.noise()
+        if self.smartFlag == 1:
+            self.smartDirection()
+        if not self.neighbors:          # following cal neighbors required
+            return
         self.cohesion()
         self.separation()
         self.alignment()
-        if self.smartFlag==1:
-            self.smartDirection()
+        print(1)
 
-    
 
     def update(self):
-        # dx = self.r1 * self.v1.x + self.r2 * self.v2.x + self.r3 * self.v3.x+ self.r4 * self.v4.x
-        # dy = self.r1 * self.v1.y + self.r2 * self.v2.y + self.r3 * self.v3.y+ self.r4 * self.v4.y
 
-        # d_pos = self.r1 * self.vf[0] + \
-        #        self.r2 * self.vf[1] + \
-        #        self.r3 * self.vf[2] + \
-        #        self.r4 * self.vf[3]
-        d_pos=sum([self.r[i] * self.vf[i] for i in range(10) ])
-
-        d_pos1=self.normalization( d_pos, self.ACCUP)
-        self.v = self.v+d_pos1
+        d_pos=self.ACCUP*sum([self.r[i] * self.vf[i] for i in range(10) ])
+        if np.linalg.norm(d_pos)>self.ACCUP:
+            d_pos1=self.normalization( d_pos, self.ACCUP)
+            self.v = self.v+d_pos1
+        else:
+            self.v = self.v +d_pos
 
         velocitynorm=np.linalg.norm(self.v)
         if velocitynorm > self.SPEED:
-            # self.vx = (self.vx / distance) * self.SPEED
-            # self.vy = (self.vy / distance) * self.SPEED
             self.v = self.SPEED / velocitynorm * self.v
-
-        # self.x += self.vx
-        # self.y += self.vy
-
         self.pos = self.pos+self.v
+        if Bird.desSquare[:2] < list(self.pos) < Bird.desSquare[2:]:
+            self.arriveFlag=1
 
         self._collision_detection()
 
-    # def clear_movement(self):
-    #     self.v1.reset()
-    #     self.v2.reset()
-    #     self.v3.reset()
 
     def draw(self, drawer):
         #self.clear_movement()
@@ -209,44 +255,37 @@ class Bird():
 
 
 def main(args):
-    Bird.birds = [Bird(args) for _ in range(Bird.NUM)]
-    for i in range(Bird.smartCount):
-        Bird.birds[i].smartFlag=1        # mark the intelligient birds
-    root = Tk()
-
-    canvas = Canvas(root, width=Field.WIDTH, height=Field.HEIGHT)
-    canvas.pack()
-
-    # birds[0].x = 50
-    # birds[0].y = 350
-    Bird.birds[0].pos=np.array([50,350])
-
-#   Bird.birds[1].x = 1
-#   Bird.birds[1].y = 100
-
-#   Bird.birds[2].x = 100
-#   Bird.birds[2].y = 1
-
-#    Bird.birds[3].x = 100
-#    Bird.birds[3].y = 100
 
     def animate():
         canvas.delete("all")
         canvas.create_rectangle(Bird.srcSquare, outline='red')
         canvas.create_rectangle(Bird.desSquare, outline='blue')
-
-  
+        #c = time.time()
+        #Bird.birds[1].dummtest()
+        # Bird.neighbors()
+        # print(time.time() - c)
         for i,bird in enumerate(Bird.birds):
+            # c = time.time()
             bird.find_neighbors()
+            # b += time.time()-c
             bird.draw(canvas)
-        
-        # Bird.birds[0].intelligentdraw(canvas)
-        # Bird.birds[1].intelligentdraw(canvas)
-        # Bird.birds[2].intelligentdraw(canvas)
-        # Bird.birds[3].intelligentdraw(canvas)
-        # Bird.birds[4].intelligentdraw(canvas)
+
+        Bird.measure()
+        #print(time.time()-c)
         root.after(20, animate)
 
+    Bird.birds = [Bird(args) for _ in range(Bird.NUM)]
+    for i in range(Bird.smartCount):
+        Bird.birds[i].smartFlag=1        # mark the intelligient birds
+    j=1
+
+    root = Tk()
+    j=j+1
+    print(j)
+    canvas = Canvas(root, width=Field.WIDTH, height=Field.HEIGHT)
+    canvas.pack()
+    # while 1:
+    #     animate()
     animate()
     root.mainloop()
 
